71fdbc84a9d5f024591b0e356fea0a06e87e6c20
0xgeeb <0xgeeb@gmail.com>
2022-03-27 22:40:04 -0400

    added odds api ability, all pro logos, and return formula

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Nav from "./components/Nav.jsx"
 import Footer from "./components/Footer.jsx"
 import Home from "./pages/Home.jsx"
 import Odds from "./pages/Odds.jsx"
+import Test from "./pages/Test.jsx"
 
 export default function App() {
     return (
@@ -13,6 +14,7 @@ export default function App() {
             <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/odds" element={<Odds />} />
+                <Route exact path="/test" element={<Test />} />
             </Routes>
             {/* <Footer /> */}
         </Router>
