24a4105a352e6e9ce686f185c39d35d3e9f2830e
0xgeeb <0xgeeb@gmail.com>
2022-05-01 05:01:37 -0400

    now able to upload past arbs to database

--- server/server.js
+++ server/server.js
@@ -3,17 +3,21 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const http = require("http");
 const axios = require('axios');
+const mongoose = require('mongoose');
+const Game = require('./model');
+const cors = require('cors');
 require("dotenv").config();
 
 const app = express();
+const server = http.createServer(app);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-const mongoose = require('mongoose');
 const dbUrl = process.env.ATLAS_URI;
+const apiKey = process.env.ODDS_APIKEY;
 
-const server = http.createServer(app);
 
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
@@ -21,28 +25,36 @@ mongoose.connect(dbUrl, {
 });
 
 const db = mongoose.connection;
+
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
   console.log("database connected");
 });
 
-app.get('/minecraftspeedrun', async (req, res) => {
-  const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-  res.send(response.data);
-})
+app.post('/minecraftspeedrun/db', async (req, res) => {
+  const game = new Game ({
+    home: {
+      name: req.body.home.name,
+      book: req.body.home.book,
+      line: req.body.home.line,
+      id: req.body.home.id
+    },
+    away: {
+      name: req.body.away.name,
+      book: req.body.away.book,
+      line: req.body.away.line,
+      id: req.body.away.id
+    },
+    arb: req.body.arb,
+    spread: req.body.spread
+  })
+  await game.save();
+});
 
 app.get('/minecraftspeedrun/bets', async (req, res) => {
-  const response = await axios.get('https://api.the-odds-api.com/v4/sports/basketball_nba/odds/?apiKey=669e626a8993cf0aa8fbd0edc9707263&regions=us&markets=h2h&oddsFormat=decimal')
-  console.log(response.data);
-})
-
-
-
-
-
-
-
-
+  const response = await axios.get(`https://api.the-odds-api.com/v4/sports/${req.query.sport}/odds/?apiKey=${apiKey}&regions=us&markets=${req.query.bet}&oddsFormat=decimal`);
+  res.send(response.data);
+});
 
 console.log("NODE_ENV is", process.env.NODE_ENV);
  
